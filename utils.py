4f7d818bb0b57a12e41e0579dea984add8b7e732
jscanass <jscanass@unal.edu.co>
2022-09-24 19:33:23 -0500

    Add examine fuction to inspect errors in categories

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def preprocessing_annotations(df_annotations):
+def preprocessing(df_annotations):
     
     """
     Preprocess annotations from a pandas DataFrame
@@ -15,17 +15,43 @@ def preprocessing_annotations(df_annotations):
     df_annotations_prepro : pandas.core.frame.DataFrame
         Dataframe preprocessed, ready for analyzing
     """
-
     df = df_annotations.copy()
     #df['min_t'] = np.floor(df['min_t'])
     #df['max_t'] = np.ceil(df['max_t'])
-    df = df.sort_values(by=['fname','min_t','max_t'],ignore_index=True)
+    df['fname'] = df['fname'].str.split('.').str[0]
     df[['site','date']] = df['fname'].str.split('_',1,expand=True)
     df['date'] = df['date'].str.split('_').apply(lambda x: x[0]+x[1])
     df['date'] = pd.to_datetime(df['date'])
-    df['annotator_label'] = list(df['label'])
-    df[['label','quality']] = df['label'].str.split('_',expand=True)
+    df[['species','quality']] = df['label'].str.split('_',expand=True)
     df['label_duration'] = df['max_t'] - df['min_t']
+    df = df.sort_values(by=['site','date','min_t','max_t'],ignore_index=True)
     df_annotations_prepro = df.copy()
 
     return df_annotations_prepro
+
+def examine(df_annotations):
+
+    """
+    Preprocess annotations from a pandas DataFrame
+    
+    Parameters
+    ----------
+    df_annotations : pandas.core.frame.DataFrame
+        DataFrame of annotations 
+    Returns
+    -------
+    df_annotations_prepro : pandas.core.frame.DataFrame
+        Dataframe preprocessed, ready for analyzing
+    """
+    
+    list_of_species = ['BOAFAB', 'PHYCUV']
+    list_of_quality = ['F','M','C']
+    df = df_annotations.copy() 
+    df = df[(~df['quality'].isin(list_of_quality))|(~df['species'].isin(list_of_species))]
+    df_annotations_errors = df.copy()
+    return df_annotations_errors
+
+
+    # Fix files ? 
+    #df['label'] = df['label'].replace({'BPAFAB':'BOAFAB','PHUCUV':'PHYCUV'})
+    #df['quality'] = df['quality'].replace({'FAR':'F','MED':'M','CLR':'C'})
